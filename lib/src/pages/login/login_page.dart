eb5a5cf0c00bb04a2e1c9620b0bf2c0cfea3e7d8
Lubpui <98448740+Lubpui@users.noreply.github.com>
2023-10-12 12:18:46 +0700

    ssd

--- lib/src/pages/login/login_page.dart
+++ lib/src/pages/login/login_page.dart
@@ -25,7 +25,7 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login Page'),
+        title: const Text('Login Pageasdasd'),
       ),
       body: Container(
         width: double.infinity,
